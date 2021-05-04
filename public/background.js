aadf265af3d7042b65b537173f3b90c46d8f5eaf
alexgonczaruk <55724886+alexgonczaruk@users.noreply.github.com>
2021-05-03 20:10:42 -0400

    shopify.js and shopify-pay.js

--- public/background.js
+++ public/background.js
@@ -1,7 +1,8 @@
-let color = '#3aa757';
 /*global chrome*/
-console.log('i ran')
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.action === 'completeCheckout') {
+		chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+			chrome.tabs.sendMessage(tabs[0].id, {action: 'completeCheckout'});
+		});
+	}
+});
